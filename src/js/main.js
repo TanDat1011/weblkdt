fdbb5878b35c6747b25ff30d3144a8e465368b34
Đạt <htdatntt@gmail.com>
2022-05-20 13:49:37 +0700

    done trang chủ index.html

--- src/js/main.js
+++ src/js/main.js
@@ -39,16 +39,16 @@
         $(this).css('background-image', 'url(' + bg + ')');
     });
 
-    //Humberger Menu
-    $(".humberger__open").on('click', function () {
-        $(".humberger__menu__wrapper").addClass("show__humberger__menu__wrapper");
-        $(".humberger__menu__overlay").addClass("active");
+    //drawer Menu
+    $(".drawer__open").on('click', function () {
+        $(".drawer__menu__wrapper").addClass("show__drawer__menu__wrapper");
+        $(".drawer__menu__overlay").addClass("active");
         $("body").addClass("over_hid");
     });
 
-    $(".humberger__menu__overlay").on('click', function () {
-        $(".humberger__menu__wrapper").removeClass("show__humberger__menu__wrapper");
-        $(".humberger__menu__overlay").removeClass("active");
+    $(".drawer__menu__overlay").on('click', function () {
+        $(".drawer__menu__wrapper").removeClass("show__drawer__menu__wrapper");
+        $(".drawer__menu__overlay").removeClass("active");
         $("body").removeClass("over_hid");
     });
 
@@ -96,8 +96,8 @@
     });
 
 
-    $('.hero__categories__all').on('click', function(){
-        $('.hero__categories ul').slideToggle(400);
+    $('.menu__categories__all').on('click', function(){
+        $('.menu__categories ul').slideToggle(400);
     });
 
     /*--------------------------
